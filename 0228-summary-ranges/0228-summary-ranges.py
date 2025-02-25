9a0ce260005be95d5f1472d5d8d4b9cac0b08304
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-02-25 11:33:40 -0500

    Time: 0 ms (100%), Space: 17.9 MB (33.73%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -2,42 +2,21 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         if len(nums)==0:
             return []
-        elif len(nums)==1:
-            return [str(nums[0])]
 
         ans=[]
         s=nums[0]
 
-        for i in range(len(nums)-1):
-            if nums[i]+1!=nums[i+1]:
-                e=nums[i]
-                if s==e:
+        for i in range(1,len(nums)):
+            if nums[i]!=nums[i-1]+1:
+                if s==nums[i-1]:
                     ans.append(str(s))
                 else:
-                    ans.append("%d->%d"%(s,e))
-                s=nums[i+1]
+                    ans.append(f"{s}->{nums[i-1]}")
+                s=nums[i]
 
-                if i==len(nums)-2:
-                    ans.append(str(s))
-            else:
-                if i==len(nums)-2:
-                    ans.append("%d->%d"%(s,nums[i+1]))
-            
+        if s==nums[-1]:
+            ans.append(str(s))
+        else:
+            ans.append(f"{s}->{nums[-1]}")
         
-
-        # for i in range(1,len(nums)):
-        #     if nums[i-1]+1!=nums[i]:
-        #         e=nums[i-1]
-        #         if s==e:
-        #             ans.append(str(s))
-        #         else:
-        #             ans.append("%d->%d"%(s,e))
-        #         s=nums[i]
-
-        #         if i==len(nums)-1:
-        #             ans.append(str(nums[-1]))
-        #     elif i==len(nums)-1:
-        #         e=nums[i]
-        #         ans.append("%d->%d"%(s,e))
-
         return ans

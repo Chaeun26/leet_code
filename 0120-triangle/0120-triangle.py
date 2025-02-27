104744c984f42ad4bb66bca2773b9b487830fafe
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-02-27 16:19:39 -0500

    Time: 3 ms (87.04%), Space: 19 MB (57.2%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -19,3 +19,11 @@ class Solution:
             ans = min(ans,dp[len(triangle[-1])-1][j])
 
         return ans
+
+        # def min_path(row,col):
+        #     path = triangle[row][col]
+        #     if row < len(triangle)-1:
+        #         path += min(min_path(row+1,col),min_path(row+1,col+1))
+        #     return path
+        
+        # return min_path(0,0)

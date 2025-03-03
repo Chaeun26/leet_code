c5627e1e060550605ac07c9c0933c7aeaab4224a
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-03 12:25:48 -0500

    Time: 0 ms (100%), Space: 18 MB (23.04%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,11 +3,28 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        # n=len(matrix)
+        # tmp = [[0]*n for _ in range(n)]
+
+        # for r in range(n):
+        #     for c in range(n):
+        #         tmp[c][n-r-1] = matrix[r][c]
+
+        # matrix[:]=tmp
+        self.transpose(matrix)
+        self.reflect(matrix)
+
+
+    def transpose(self, matrix):
         n=len(matrix)
-        tmp = [[0]*n for _ in range(n)]
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[j][i],matrix[i][j] = matrix[i][j], matrix[j][i]
+    
+    def reflect(self, matrix):
+        n=len(matrix)
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j],matrix[i][-j-1] = matrix[i][-j-1],matrix[i][j]
 
-        for r in range(n):
-            for c in range(n):
-                tmp[c][n-r-1] = matrix[r][c]
 
-        matrix[:]=tmp

4ed6dda1eec2eda93deae2ad552f547f6c67c43d
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-04 18:32:34 -0500

    Time: 0 ms (100%), Space: 17.7 MB (98.77%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -6,20 +6,37 @@ class Solution:
         m=len(obstacleGrid)
         n=len(obstacleGrid[0])
 
-        if m==1 and n==1:
-            return 1
-        elif m==2 and n==1:
-            return 1 if obstacleGrid[0][0]==0 and obstacleGrid[1][0]==0 else 0
-        elif m==1 and n==2:
-            return 1 if obstacleGrid[0][0]==0 and obstacleGrid[0][1]==0 else 0
+        obstacleGrid[0][0]=1
 
-        path=[[0]*(n+1) for _ in range(m+1)]
+        for i in range(1,m):
+            obstacleGrid[i][0] = int(obstacleGrid[i][0]== 0 and obstacleGrid[i-1][0]==1)
 
-        path[1][1]=1
+        for j in range(1,n):
+            obstacleGrid[0][j] = int(obstacleGrid[0][j]==0 and obstacleGrid[0][j-1]==1)
 
-        for r in range(1,m+1):
-            for c in range(1,n+1):
-                if obstacleGrid[r-1][c-1]!=1:
-                    path[r][c]+=path[r-1][c]+path[r][c-1]
+        for i in range(1,m):
+            for j in range(1,n):
+                if obstacleGrid[i][j]==0:
+                    obstacleGrid[i][j] = (obstacleGrid[i-1][j] + obstacleGrid[i][j-1])
+                else:
+                    obstacleGrid[i][j]=0
+        
+        return obstacleGrid[m-1][n-1]
 
-        return path[m][n]
+        # if m==1 and n==1:
+        #     return 1
+        # elif m==2 and n==1:
+        #     return 1 if obstacleGrid[0][0]==0 and obstacleGrid[1][0]==0 else 0
+        # elif m==1 and n==2:
+        #     return 1 if obstacleGrid[0][0]==0 and obstacleGrid[0][1]==0 else 0
+
+        # path=[[0]*(n+1) for _ in range(m+1)]
+
+        # path[1][1]=1
+
+        # for r in range(1,m+1):
+        #     for c in range(1,n+1):
+        #         if obstacleGrid[r-1][c-1]!=1:
+        #             path[r][c]+=path[r-1][c]+path[r][c-1]
+
+        # return path[m][n]

aee349a526f2389723598296f1c0afd26a94ae6f
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-09 16:32:11 -0400

    Time: 1 ms (92.81%), Space: 18.5 MB (19.38%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -4,27 +4,51 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import defaultdict
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        count=0
-        stack=[(root, [])]
+        # count=0
+        # stack=[(root, [])]
         
-        while stack:
-            node,sums=stack.pop()
+        # while stack:
+        #     node,sums=stack.pop()
             
-            if node:
-                tmp=[]
-                tmp.append(node.val)
-                for s in sums:
-                    tmp.append(s+node.val)
+        #     if node:
+        #         tmp=[]
+        #         tmp.append(node.val)
+        #         for s in sums:
+        #             tmp.append(s+node.val)
 
-                    if s+node.val==targetSum:
-                        print(node.val)
-                        count+=1
-                if node.val==targetSum:
-                    count+=1
+        #             if s+node.val==targetSum:
+        #                 print(node.val)
+        #                 count+=1
+        #         if node.val==targetSum:
+        #             count+=1
                 
-                stack.append((node.left,tmp))
-                stack.append((node.right,tmp))
+        #         stack.append((node.left,tmp))
+        #         stack.append((node.right,tmp))
                 
+        # return count
+        count=0
+        def preorder(node,curr_sum):
+            nonlocal count
+            if not node:
+                return
+            
+            curr_sum+=node.val
+
+            if curr_sum==targetSum:
+                count+=1
+            
+            count+=h[curr_sum-targetSum]
+
+            h[curr_sum]+=1
+
+            preorder(node.left,curr_sum)
+            preorder(node.right,curr_sum)
+
+            h[curr_sum]-=1
+        
+        h=defaultdict(int)
+        preorder(root,0)
         return count

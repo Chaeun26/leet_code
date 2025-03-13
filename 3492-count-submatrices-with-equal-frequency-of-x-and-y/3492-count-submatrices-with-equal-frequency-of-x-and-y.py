2376081a3d80479744de7d7e03ee347ca77172e7
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-13 15:03:11 -0400

    Time: 427 ms (90.43%), Space: 118.4 MB (89.36%) - LeetHub

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/3492-count-submatrices-with-equal-frequency-of-x-and-y.py
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/3492-count-submatrices-with-equal-frequency-of-x-and-y.py
@@ -3,32 +3,30 @@ class Solution:
         
         m,n=len(grid),len(grid[0])
         ans=0
-        dp=[[0]*n for _ in range(m)]
-        dp2=[[False]*n for _ in range(m)]
-
         for r in range(m):
             for c in range(n):
                 if grid[r][c]=="X":
                     grid[r][c]=1
-                    dp2[r][c]=True
                 elif grid[r][c]=="Y":
                     grid[r][c]=-1
                 else:
                     grid[r][c]=0
-                
-                dp[r][c]=grid[r][c]
 
-                if r==0 and c>0:
-                    dp[r][c]+=dp[r][c-1]
-                    dp2[r][c]=dp2[r][c] or dp2[r][c-1]
-                elif c==0 and r>0:
-                    dp[r][c]+=dp[r-1][c]
-                    dp2[r][c]=dp2[r][c] or dp2[r-1][c]
-                elif r>0 and c>0:
-                    dp[r][c]+=dp[r-1][c]+dp[r][c-1]-dp[r-1][c-1]
-                    dp2[r][c]=dp2[r][c] or dp2[r-1][c] or dp2[r][c-1]
+        col_sum=[0]*n
+        is_contain=[False]*n
+        for r in range(m):
+            for c in range(n):
+                col_sum[c]+=grid[r][c]
+                if grid[r][c]==1:
+                    is_contain[c]=True
 
-                if dp[r][c]==0 and dp2[r][c]:
-                    ans+=1
+            prefix_sum=0
+            for c in range(n):
+                prefix_sum+=col_sum[c]
+                if prefix_sum==0:
+                    for i in range(c+1):
+                        if is_contain[i]:
+                            ans+=1
+                            break
 
         return ans

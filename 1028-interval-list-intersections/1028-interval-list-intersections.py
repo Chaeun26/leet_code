7902cccdf1c8bdda9c6927bd0e583a3ebe3ca4bb
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-15 13:31:41 -0400

    Time: 0 ms (100%), Space: 18.8 MB (23.53%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.py
+++ 1028-interval-list-intersections/1028-interval-list-intersections.py
@@ -1,18 +1,46 @@
 from collections import deque
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        ans=[]
-        i=j=0
-
-        while i<len(firstList) and j<len(secondList):
-            lo=max(firstList[i][0],secondList[j][0])
-            hi=min(firstList[i][1],secondList[j][1])
-            if lo<=hi:
-                ans.append([lo,hi])
-
-            if firstList[i][1]<secondList[j][1]:
-                i+=1
-            else:
-                j+=1
+        if len(firstList)==0 or len(secondList)==0:
+            return []
         
+        firstList=deque(firstList)
+        secondList=deque(secondList)
+        ans=[]
+
+        first_s,first_e=firstList.popleft()
+        second_s,second_e=secondList.popleft()
+        
+        while True:
+            if first_e>=second_s and second_e>=first_s:
+                ans.append([max(first_s,second_s),min(first_e,second_e)])
+
+
+            if firstList and first_e<second_e:
+                first_s,first_e=firstList.popleft()
+            elif secondList and second_e<first_e:
+                second_s,second_e=secondList.popleft()
+            elif firstList and secondList:
+                first_s,first_e=firstList.popleft()
+                second_s,second_e=secondList.popleft()
+            else:
+                break
+
+                
         return ans
+
+        # ans=[]
+        # i=j=0
+
+        # while i<len(firstList) and j<len(secondList):
+        #     lo=max(firstList[i][0],secondList[j][0])
+        #     hi=min(firstList[i][1],secondList[j][1])
+        #     if lo<=hi:
+        #         ans.append([lo,hi])
+
+        #     if firstList[i][1]<secondList[j][1]:
+        #         i+=1
+        #     else:
+        #         j+=1
+        
+        # return ans

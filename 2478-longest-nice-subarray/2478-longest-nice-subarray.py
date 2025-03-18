d6b4ab28710636ba2a7e71ea45d930c954b18ff6
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-18 18:15:58 -0400

    Time: 108 ms (31.91%), Space: 31.7 MB (82.37%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.py
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.py
@@ -1,10 +1,9 @@
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
         ans=1
-        curr=nums[0]
-        j=0
+        curr=j=0
 
-        for i in range(1,len(nums)):
+        for i in range(len(nums)):
             while (nums[i] & curr) != 0 and j<len(nums):
                 curr=curr^nums[j]
                 j+=1

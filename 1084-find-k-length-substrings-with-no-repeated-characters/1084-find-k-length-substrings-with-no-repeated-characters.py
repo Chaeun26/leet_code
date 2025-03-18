8277af8a5ccd5cfabe4ba1017ffdf2b9215425d5
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-18 18:41:46 -0400

    Time: 7 ms (93.65%), Space: 17.6 MB (95.59%) - LeetHub

--- 1084-find-k-length-substrings-with-no-repeated-characters/1084-find-k-length-substrings-with-no-repeated-characters.py
+++ 1084-find-k-length-substrings-with-no-repeated-characters/1084-find-k-length-substrings-with-no-repeated-characters.py
@@ -1,10 +1,19 @@
 class Solution:
     def numKLenSubstrNoRepeats(self, s: str, k: int) -> int:
-        count=0
-        
-        for i in range(len(s)-k+1):
-            tmp=set(s[i:i+k])
+        j=count=0
+        tmp=set()
+
+        for i in range(len(s)):
+      
+            while s[i] in tmp or len(tmp)>=k:
+                tmp.remove(s[j])
+                j+=1
+
+            if s[i] not in tmp:
+                tmp.add(s[i])
+
             if len(tmp)==k:
                 count+=1
+                
 
         return count

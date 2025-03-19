cb591e13ef425dc36d4a33955fd4cf22c3c1a71f
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-19 15:24:47 -0400

    Time: 111 ms (36.26%), Space: 18 MB (68.49%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.py
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         n=len(s)
-        dp=[0]*51
+        dp=[0]*(n+1)
 
         for i in range(n-1,-1,-1):
             dp[i]=dp[i+1]+1

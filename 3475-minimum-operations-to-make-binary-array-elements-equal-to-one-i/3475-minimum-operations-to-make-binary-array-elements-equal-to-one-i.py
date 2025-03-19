fc351d9f67dfbc6053c38f9c22514524ac8c23b8
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-19 10:30:58 -0400

    Time: 99 ms (69.92%), Space: 21.7 MB (20.05%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,12 +1,13 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
        count=0
+       n=len(nums)
 
-       for i in range(len(nums)-2):
+       for i in range(n-2):
             if nums[i]==0:
                 nums[i]=1
                 nums[i+1]^=1
                 nums[i+2]^=1
                 count+=1
-        
-       return count if sum(nums)==len(nums) else -1
+       
+       return -1 if nums[n-2]==0 or nums[n-1]==0 else count

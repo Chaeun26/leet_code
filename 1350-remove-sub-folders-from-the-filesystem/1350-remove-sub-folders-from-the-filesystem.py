6f18ee2e0a3b1dee54d2ced674d06e047b05b3ba
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-20 10:17:41 -0400

    Time: 191 ms (11.52%), Space: 43.7 MB (16.85%) - LeetHub

---
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,33 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        root=TrieNode()
+
+        for f in folder:
+            components=f.strip("/").split("/")
+            node=root
+            for c in components:
+                if c not in node.children:
+                    node.children[c] = TrieNode()
+                node = node.children[c]
+            node.is_end=True
+
+        def dfs(node,components):
+            if node.is_end:
+                return False if len(components)>0 else True
+            
+            c=components[0]
+
+            return dfs(node.children[c],components[1:])
+
+        ans=[]
+        
+        for f in folder:
+            components=f.strip("/").split("/")
+            if dfs(root,components):
+                ans.append(f)
+
+        return ans

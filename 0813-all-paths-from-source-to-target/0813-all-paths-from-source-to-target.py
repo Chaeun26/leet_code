44314dfc2a1f2c4b55a17a882e4cbd6ac13f4be6
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-23 10:51:18 -0400

    Time: 7 ms (44.22%), Space: 19.3 MB (30.19%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -1,17 +1,35 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        n=len(graph)
+        # n=len(graph)
         
-        queue=deque([(0,[0])])
+        # queue=deque([(0,[0])])
+        # ans=[]
+
+        # while queue:
+        #     node,path=queue.popleft()
+
+        #     if node==n-1:
+        #         ans.append(path)
+
+        #     for neighbor in graph[node]:
+        #         queue.append((neighbor,path+[neighbor]))
+
+        # return ans
+
+        target=len(graph)-1
         ans=[]
 
-        while queue:
-            node,path=queue.popleft()
-
-            if node==n-1:
-                ans.append(path)
-
-            for neighbor in graph[node]:
-                queue.append((neighbor,path+[neighbor]))
+        def backtracking(curr_node, path):
+            if curr_node==target:
+                ans.append(list(path))
+                return
+            
+            for next_node in graph[curr_node]:
+                path.append(next_node)
+                backtracking(next_node,path)
+                path.pop()
+            
+        path=[0]
+        backtracking(0,path)
 
         return ans

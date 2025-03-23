041f27eb7bfef5ef43aa7dca3339f25bae876a06
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-03-23 10:44:34 -0400

    Time: 1 ms (96.89%), Space: 19.2 MB (67.35%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -1,10 +1,6 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        connections=defaultdict(list)
         n=len(graph)
-
-        for i,neighbor in enumerate(graph):
-            connections[i]=neighbor
         
         queue=deque([(0,[0])])
         ans=[]

0dc810c3bbe3080bb763e3a149d1599ef9dbf7a7
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-04-02 16:32:28 -0400

    Time: 7 ms (95.68%), Space: 31.1 MB (33.34%) - LeetHub

--- 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
@@ -1,12 +1,13 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         n=len(heights)
-        ans = deque()
+        ans = []
         curr_max = -inf
 
         for i in range(n-1,-1,-1):
             if curr_max < heights[i]:
                 curr_max = heights[i]
-                ans.appendleft(i)
-        
-        return list(ans)
+                ans.append(i)
+                
+        ans.reverse()
+        return ans

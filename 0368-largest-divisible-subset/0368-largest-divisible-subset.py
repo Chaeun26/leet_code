a898d2aa2e93bbfd54a4279819e3b656aaafef8f
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-04-06 11:11:47 -0400

    Time: 247 ms (16.57%), Space: 17.8 MB (92.2%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -0,0 +1,50 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+
+        nums.sort()
+        prev=[-1]*n
+        dp=[1]*n
+        max_idx=0
+
+        for i in range(1,n):
+            for j in range(i):
+                if nums[i]%nums[j]==0 and dp[i] < dp[j] + 1:
+                    prev[i]=j
+                    dp[i]=dp[j]+1
+                if dp[i] > dp[max_idx]:
+                    max_idx = i
+
+        res=[]
+        k=max_idx
+        while k>=0:
+            res.append(nums[k])
+            k=prev[k]
+
+        return res
+        '''
+        3 4 8 16
+        # backtracking - TLE occurs
+
+        n=len(nums)
+        nums.sort()
+        self.max_len=0
+        self.res=[]
+        
+        def backtrack(idx,curr):
+            if idx==n:
+                if len(curr)>self.max_len:
+                    self.max_len=len(curr)
+                    self.res=curr[:]
+                return 
+
+            if not curr or nums[idx]%curr[-1]==0:
+                curr.append(nums[idx])
+                backtrack(idx+1, curr)
+                curr.pop()
+
+            backtrack(idx+1, curr)
+
+        backtrack(0,[])
+        return self.res
+        '''

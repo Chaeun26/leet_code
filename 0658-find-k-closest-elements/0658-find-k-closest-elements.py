abbe3feb9cf6e41eb4be19d67d269b6a4b622c3b
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-04-19 21:12:16 -0400

    Time: 7 ms (64.92%), Space: 19 MB (83.15%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -4,24 +4,42 @@ class Solution:
         1 2 3 4 5
         2 1 0 1 2
 
-        1 2 3 4 k=4
+        left=1 (2) -> 0 (1)
+        right=2 (3) -> 3 (4) -> 4 (5)
 
         '''
+        if len(arr)==k:
+            return arr
         
-        ans=[]
+        left=bisect_left(arr,x)-1
+        right=left+1
 
-        for num in arr:
-            diff=abs(num-x)
-            if k>0:
-                heapq.heappush(ans,(-diff, num))
-                k-=1
+        while right-left-1 < k:
+            if left==-1:
+                right+=1
+                continue
+
+            if right==len(arr) or abs(arr[left]-x) <= abs(arr[right]-x):
+                left-=1
             else:
-                max_diff, _=ans[0]
-                if diff < -max_diff:
-                    heapq.heappop(ans)
-                    heapq.heappush(ans,(-diff,num))
+                right+=1
 
-        ans = [val for _,val in ans]
-        ans.sort()
+        return arr[left+1:right]
+        
+        # ans=[]
 
-        return ans
+        # for num in arr:
+        #     diff=abs(num-x)
+        #     if k>0:
+        #         heapq.heappush(ans,(-diff, num))
+        #         k-=1
+        #     else:
+        #         max_diff, _=ans[0]
+        #         if diff < -max_diff:
+        #             heapq.heappop(ans)
+        #             heapq.heappush(ans,(-diff,num))
+
+        # ans = [val for _,val in ans]
+        # ans.sort()
+
+        # return ans

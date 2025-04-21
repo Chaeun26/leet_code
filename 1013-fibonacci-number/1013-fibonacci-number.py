685c36011558dd17ab8d4fa6b4bdcf8f52ef9b06
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-04-21 12:00:07 -0400

    Time: 38 ms (67.55%), Space: 17.9 MB (24.32%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -3,4 +3,12 @@ class Solution:
         if n<=1:
             return n
 
-        return self.fib(n-1)+self.fib(n-2)
+        ans,prev=1,0
+
+        for i in range(n-1):
+            tmp=ans
+            ans+=prev
+            prev=tmp
+
+        
+        return ans

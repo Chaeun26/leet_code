b0e01f2b9c2cc0dc3c09f83b51927a741dde70dc
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-04-23 14:55:14 -0400

    Time: 58 ms (28.05%), Space: 21.1 MB (35.13%) - LeetHub

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -8,26 +8,48 @@ class Node:
         self.parent = None
 """
 
+# class Solution:
+#     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+#         parents=set()
+#         parents.add(p.val)
+#         parents.add(q.val)
+
+#         while p or q:
+#             if p and p.parent:
+#                 if p.parent.val in parents:
+#                     return p.parent
+#                 parents.add(p.parent.val)
+#                 p=p.parent
+#             if q and q.parent:
+#                 if q.parent.val in parents:
+#                     return q.parent
+#                 parents.add(q.parent.val)
+#                 q=q.parent
+
+#         '''
+#                 1
+#             2            3
+#                 4           
+#         '''
+
 class Solution:
-    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        parents=set()
-        parents.add(p.val)
-        parents.add(q.val)
+    def get_depth(self,node:'Node') -> int:
+        depth=0
+        while node:
+            node=node.parent
+            depth+=1
+        return depth
+    def lowestCommonAncestor(self, p:'Node', q:'Node') -> 'Node':
+        p_depth=self.get_depth(p)
+        q_depth=self.get_depth(q)
 
-        while p or q:
-            if p and p.parent:
-                if p.parent.val in parents:
-                    return p.parent
-                parents.add(p.parent.val)
-                p=p.parent
-            if q and q.parent:
-                if q.parent.val in parents:
-                    return q.parent
-                parents.add(q.parent.val)
-                q=q.parent
-
-        '''
-                1
-            2            3
-                4           
-        '''
+        for i in range(p_depth-q_depth):
+            p=p.parent
+        for i in range(q_depth-p_depth):
+            q=q.parent
+        
+        while p!=q:
+            p=p.parent
+            q=q.parent
+        
+        return p

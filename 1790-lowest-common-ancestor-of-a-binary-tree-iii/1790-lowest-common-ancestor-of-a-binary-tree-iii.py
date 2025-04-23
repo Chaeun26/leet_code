8aca706a672f3ad17afd8ef7b877f69faa5173dd
Chaeun Hong <75800865+Chaeun26@users.noreply.github.com>
2025-04-23 14:55:28 -0400

    Time: 55 ms (46.72%), Space: 21 MB (56.58%) - LeetHub

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -8,23 +8,23 @@ class Node:
         self.parent = None
 """
 
-# class Solution:
-#     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-#         parents=set()
-#         parents.add(p.val)
-#         parents.add(q.val)
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        parents=set()
+        parents.add(p.val)
+        parents.add(q.val)
 
-#         while p or q:
-#             if p and p.parent:
-#                 if p.parent.val in parents:
-#                     return p.parent
-#                 parents.add(p.parent.val)
-#                 p=p.parent
-#             if q and q.parent:
-#                 if q.parent.val in parents:
-#                     return q.parent
-#                 parents.add(q.parent.val)
-#                 q=q.parent
+        while p or q:
+            if p and p.parent:
+                if p.parent.val in parents:
+                    return p.parent
+                parents.add(p.parent.val)
+                p=p.parent
+            if q and q.parent:
+                if q.parent.val in parents:
+                    return q.parent
+                parents.add(q.parent.val)
+                q=q.parent
 
 #         '''
 #                 1
@@ -32,24 +32,24 @@ class Node:
 #                 4           
 #         '''
 
-class Solution:
-    def get_depth(self,node:'Node') -> int:
-        depth=0
-        while node:
-            node=node.parent
-            depth+=1
-        return depth
-    def lowestCommonAncestor(self, p:'Node', q:'Node') -> 'Node':
-        p_depth=self.get_depth(p)
-        q_depth=self.get_depth(q)
+# class Solution:
+#     def get_depth(self,node:'Node') -> int:
+#         depth=0
+#         while node:
+#             node=node.parent
+#             depth+=1
+#         return depth
+#     def lowestCommonAncestor(self, p:'Node', q:'Node') -> 'Node':
+#         p_depth=self.get_depth(p)
+#         q_depth=self.get_depth(q)
 
-        for i in range(p_depth-q_depth):
-            p=p.parent
-        for i in range(q_depth-p_depth):
-            q=q.parent
+#         for i in range(p_depth-q_depth):
+#             p=p.parent
+#         for i in range(q_depth-p_depth):
+#             q=q.parent
         
-        while p!=q:
-            p=p.parent
-            q=q.parent
+#         while p!=q:
+#             p=p.parent
+#             q=q.parent
         
-        return p
+#         return p
